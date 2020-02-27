a061641239f6eac92d23e9c1f360eb6f3f3f8af5
a3drian <38135936+a3drian@users.noreply.github.com>
2020-02-27 20:58:44 +0200

    Lab 1 exercise files for string1 and list1. TO DO: wordcount.py

---
+++ LAB/Lab1/Intro/list1.py
@@ -0,0 +1,107 @@
+# ISML, Laborator 1
+# list1.py
+# Manghiuc Teodor-Adrian
+# 382
+
+# A. match_ends [DONE]
+# Given a list of strings, return the count of the number of
+# strings where the string length is 2 or more and the first
+# and last chars of the string are the same.
+# Note: python does not have a ++ operator, but += works.
+def match_ends(words):
+    # +++your code here+++
+    index = 0
+    counter = 0
+    while index < len(words):
+        temp = words[index]
+        # print(temp)
+        if len(temp) != 0:
+            a = temp[0]
+            z = temp[-1]
+            if a == z:
+                if len(temp) >= 2:
+                    counter += 1
+        index += 1
+    return counter
+
+
+# B. front_x [DONE]
+# Given a list of strings, return a list with the strings
+# in sorted order, except group all the strings that begin with 'x' first.
+# e.g. ['mix', 'xyz', 'apple', 'xanadu', 'aardvark'] yields
+# ['xanadu', 'xyz', 'aardvark', 'apple', 'mix']
+# Hint: this can be done by making 2 lists and sorting each of them
+# before combining them.
+def front_x(words):
+    # +++your code here+++
+    o_temp = []
+    x_temp = []
+    for word in words:
+        if word[0] == 'x':
+            x_temp.append(word)
+        else:
+            o_temp.append(word)
+    x_temp.sort()
+    o_temp.sort()
+    # print(o_temp)
+    # print(x_temp)
+    return x_temp + o_temp
+
+
+# C. sort_last [DONE]
+# Given a list of non-empty tuples, return a list sorted in increasing
+# order by the last element in each tuple.
+# e.g. [(1, 7), (1, 3), (3, 4, 5), (2, 2)] yields
+# [(2, 2), (1, 3), (3, 4, 5), (1, 7)]
+# Hint: use a custom key= function to extract the last element form each tuple.
+def extract(tuple):
+    # print(tuple[-1])
+    return tuple[-1]
+
+
+def sort_last(tuples):
+    # +++your code here+++
+    for tuple in tuples:
+        extract(tuple)
+        s = sorted(tuples, key=extract)
+    return s
+
+
+# Simple provided test() function used in main() to print
+# what each function returns vs. what it's supposed to return.
+def test(got, expected):
+    if got == expected:
+        prefix = ' OK '
+    else:
+        prefix = '  X '
+    print('%s got: %s expected: %s' % (prefix, repr(got), repr(expected)))
+
+
+# Calls the above functions with interesting inputs.
+def main():
+    print('match_ends')
+    test(match_ends(['aba', 'xyz', 'aa', 'x', 'bbb']), 3)
+    test(match_ends(['', 'x', 'xy', 'xyx', 'xx']), 2)
+    test(match_ends(['aaa', 'be', 'abc', 'hello']), 1)
+
+    print()
+    print('front_x')
+    test(front_x(['bbb', 'ccc', 'axx', 'xzz', 'xaa']),
+         ['xaa', 'xzz', 'axx', 'bbb', 'ccc'])
+    test(front_x(['ccc', 'bbb', 'aaa', 'xcc', 'xaa']),
+         ['xaa', 'xcc', 'aaa', 'bbb', 'ccc'])
+    test(front_x(['mix', 'xyz', 'apple', 'xanadu', 'aardvark']),
+         ['xanadu', 'xyz', 'aardvark', 'apple', 'mix'])
+
+    print()
+    print('sort_last')
+    test(sort_last([(1, 3), (3, 2), (2, 1)]),
+         [(2, 1), (3, 2), (1, 3)])
+    test(sort_last([(2, 3), (1, 2), (3, 1)]),
+         [(3, 1), (1, 2), (2, 3)])
+    test(sort_last([(1, 7), (1, 3), (3, 4, 5), (2, 2)]),
+         [(2, 2), (1, 3), (3, 4, 5), (1, 7)])
+
+
+if __name__ == '__main__':
+    main()
---
+++ LAB/Lab1/Intro/string1.py
@@ -0,0 +1,119 @@
+# ISML, Laborator 1
+# string1.py
+# Manghiuc Teodor-Adrian
+# 382
+
+# A. donuts [DONE]
+# Given an int count of a number of donuts, return a string
+# of the form 'Number of donuts: <count>', where <count> is the number
+# passed in. However, if the count is 10 or more, then use the word 'many'
+# instead of the actual count.
+# So donuts(5) returns 'Number of donuts: 5'
+# and donuts(23) returns 'Number of donuts: many'
+def donuts(count):
+    # +++your code here+++
+    if count >= 10:
+      return "Number of donuts: many"
+    else:
+      return "Number of donuts: " + str(count)
+
+
+# B. both_ends [DONE]
+# Given a string s, return a string made of the first 2
+# and the last 2 chars of the original string,
+# so 'spring' yields 'spng'. However, if the string length
+# is less than 2, return instead the empty string.
+def both_ends(s):
+    # +++your code here+++
+    if len(s) < 2:
+      return ""
+    else:
+      begin = s[0] + s[1]
+      last = s[-2] + s[-1]
+      return begin + last
+
+
+# C. fix_start [DONE]
+# Given a string s, return a string
+# where all occurences of its first char have
+# been changed to '*', except do not change
+# the first char itself.
+# e.g. 'babble' yields 'ba**le'
+# Assume that the string is length 1 or more.
+# Hint: s.replace(stra, strb) returns a version of string s
+# where all instances of stra have been replaced by strb.
+def fix_start(s):
+    # +++your code here+++
+    x = s[0]
+    temp = s[1:]
+    final = x
+    for i in temp:
+      if i == x:
+        final = final + "*"
+      else:
+        final = final + i
+    return final
+
+
+# D. MixUp [DONE]
+# Given strings a and b, return a single string with a and b separated
+# by a space '<a> <b>', except swap the first 2 chars of each string.
+# e.g.
+#   'mix', pod' -> 'pox mid'
+#   'dog', 'dinner' -> 'dig donner'
+# Assume a and b are length 2 or more.
+def mix_up(a, b):
+    # +++your code here+++
+    x = a[0] + a[1]
+    y = b[0] + b[1]
+    n = y + a[2:]
+    m = x + b[2:]
+    return n + " " + m
+
+
+# Provided simple test() function used in main() to print
+# what each function returns vs. what it's supposed to return.
+def test(got, expected):
+    if got == expected:
+        prefix = ' OK '
+    else:
+        prefix = '  X '
+    print('%s got: %s expected: %s' % (prefix, repr(got), repr(expected)))
+
+    # Provided main() calls the above functions with interesting inputs,
+    # using test() to check if each result is correct or not.
+
+
+def main():
+    print('donuts')
+    # Each line calls donuts, compares its result to the expected for that call.
+    test(donuts(4), 'Number of donuts: 4')
+    test(donuts(9), 'Number of donuts: 9')
+    test(donuts(10), 'Number of donuts: many')
+    test(donuts(99), 'Number of donuts: many')
+
+    print()
+    print('both_ends')
+    test(both_ends('spring'), 'spng')
+    test(both_ends('Hello'), 'Helo')
+    test(both_ends('a'), '')
+    test(both_ends('xyz'), 'xyyz')
+
+    print()
+    print('fix_start')
+    test(fix_start('babble'), 'ba**le')
+    test(fix_start('aardvark'), 'a*rdv*rk')
+    test(fix_start('google'), 'goo*le')
+    test(fix_start('donut'), 'donut')
+
+    print()
+    print('mix_up')
+    test(mix_up('mix', 'pod'), 'pox mid')
+    test(mix_up('dog', 'dinner'), 'dig donner')
+    test(mix_up('gnash', 'sport'), 'spash gnort')
+    test(mix_up('pezzy', 'firm'), 'fizzy perm')
+
+
+# Standard boilerplate to call the main() function.
+if __name__ == '__main__':
+    main()
